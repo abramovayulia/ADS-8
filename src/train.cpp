9b2c0560d5c6fdb7472fb3f891d072acd3f7c174
abramovayulia <116936584+abramovayulia@users.noreply.github.com>
2023-05-13 17:51:34 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,44 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+void Train::addCage(bool light) {
+  if (!head) {
+    Cage* item = new Cage;
+    item->light = light;
+    first = item;
+    tail = item;
+  } else {
+    Cage* item = new Cage;
+    item->light = light;
+    tail->next = item;
+    item->prev = tail;
+    tail = item;
+  }
+  tail->next = first;
+  first->prev = tail;
+}
+
+int Train::getLength() {
+  Cage* temp = first;
+  temp->light = true;
+  while (true) {
+    temp = temp->next;
+    count1++;
+    if (temp->light) {
+      temp->light = false;
+      for (int i = count1; i > 0; i--) {
+        temp = temp->prev;
+      }
+      countOp += count1 * 2;
+      if (!temp->light) {
+        return count1;
+      }
+      count1 = 0;
+    }
+  }
+  return 0;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
